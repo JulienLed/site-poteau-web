a5f5902f30bcb9a0dde9ccab64eca8cc71305255
Stud <ledentj@gmail.com>
2025-10-10 17:46:36 +0200

    Add theme of portfolio by phone in Au Laboureur bar in Brussels

--- app/portfolio/portfolio.tsx
+++ app/portfolio/portfolio.tsx
@@ -1,9 +1,9 @@
 export default function Portfolio() {
   return (
     <div>
-      <section id="project-1"></section>
-      <section id="project-2"></section>
-      <section id="project-3"></section>
+      <section id="project-1"><div>Le site d'une crèche</div></section>
+      <section id="project-2"><div>Le site d'une boulangerie</div></section>
+      <section id="project-3"><div>Le site d'une société de logements sociaux</div></section>
     </div>
   );
 }
