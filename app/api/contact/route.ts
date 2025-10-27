56e1efe86622d970dc31bda6242fdbab09766ba3
Julien LEDENT <ledentj@gmail.com>
2025-10-27 15:55:16 +0100

    Change Nodemailer to Resend

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,31 +1,20 @@
 "use server";
 
 import { NextResponse } from "next/server";
-import nodemailer from "nodemailer";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req: Request) {
   try {
-    const transporter = nodemailer.createTransport({
-      host: "smtp.mail.ovh.net",
-      port: 465,
-      secure: true,
-      auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS,
-      },
-    });
-
     const { name, email, phone, message } = await req.json();
 
-    await transporter.sendMail({
-      from: process.env.SMTP_USER, // toujours ton compte OVH
-      replyTo: `${name} <${email}>`, // le client peut répondre
-      to: process.env.SMTP_USER,
-      subject: `Nouveau message de ${name} - ${phone}`,
-      text: message,
-      html: `<p>${message}</p>`,
+    resend.emails.send({
+      from: "contact@lepoteauduweb.be",
+      to: "contact@lepoteauduweb.be",
+      subject: `Demande de contact reçue de ${name} - mail:${email} - tel:${phone} `,
+      html: message,
     });
-
     return NextResponse.json({ success: true });
   } catch (error) {
     return NextResponse.json({ success: false, error: String(error) });
