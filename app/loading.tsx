a9f25e8cc8de6fa6977cdc3227e2e041f9f8f094
Julien LEDENT <ledentj@gmail.com>
2026-03-04 12:12:26 +0100

    Delete loading

--- app/loading.tsx
+++
@@ -1,8 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="w-full min-h-screen flex flex-col gap-5 justify-center items-center">
-      <div className="loader"></div>
-      <p className="!text-logo-blue font-bold">Chargement du contenu...</p>
-    </div>
-  );
-}
